612e5233845fdc889488539d735af9e42dd07954
mihalyszijjartonagy <mihaly.szijjartonagy@prezi.com>
2020-05-06 15:34:16 +0200

    responding 404

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 
 from polls.models import Question
 
 
 def index(request):
     questions = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    # context = {'questions': questions}
-    # return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', {'questions': questions})
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question #%s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
