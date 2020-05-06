06900af9d095ffa49a38d4aca3ae21f9cdbdfa9d
mihalyszijjartonagy <mihaly.szijjartonagy@prezi.com>
2020-05-06 10:24:06 +0200

    initial commit

---
+++ polls/models.py
@@ -0,0 +1,26 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question_text
+        # return "#%d - %s" % (self.id, self.question_text)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
+        # return "#%d - %s (question#%d)" % (self.id, self.choice_text, self.question.id)
