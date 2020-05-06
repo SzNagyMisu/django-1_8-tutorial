373322bde6035025b37fbc5d43bad8ac00b5e960
mihalyszijjartonagy <mihaly.szijjartonagy@prezi.com>
2020-05-06 14:31:34 +0200

    first view

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import polls.urls
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^polls/', include(polls.urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
